6a1a9929883ddcc7f8b58cc7e6a8478eebb4a4e5
alcorcamel <remibourdon@free.fr>
2026-01-06 21:53:10 +0100

    ebauche du main

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: demane <emanedanielakim@gmail.com>         +#+  +:+       +#+        */
+/*   By: rbourdon <rbourdon@student.42paris.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 13:24:02 by demane            #+#    #+#             */
-/*   Updated: 2026/01/06 14:06:33 by demane           ###   ########.fr       */
+/*   Updated: 2026/01/06 21:52:32 by rbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "includes/minishell remi.h"
+
+int	ft_minishell(int ac, char **av)
+{
+	char	*line;
+	t_token	*tokens;
+
+	while (1)
+	{
+		line = read_line();
+		if (!line)
+			return (1);// en realite erreur sans quitter
+		//fonction pour ajouter a l'historique: ft_add_history(line);
+		tokens = ft_lexer(line);
+		free(line);
+		/*
+		parser
+		executeur
+		*/
+
+
+
+	}
+
+}
