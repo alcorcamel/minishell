6048f9c96bf7ac82f3d759c2424a0fbfaf7a5c0d
Daniel Akim Emane <demane@f6r8s15.clusters.42paris.fr>
2026-01-06 14:06:33 +0100

    init

---
+++ includes/minishell.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: demane <emanedanielakim@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 13:23:59 by demane            #+#    #+#             */
+/*   Updated: 2026/01/06 13:47:08 by demane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef enum e_token_type {
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_AND,
+	TOKEN_OR
+}	t_token_type;
+
+typedef struct s_token {
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
+typedef enum e_node_type {
+	NODE_CMD,
+	NODE_PIPE,
+	NODE_REDIR_IN,
+	NODE_REDIR_OUT,
+	NODE_REDIR_APPEND,
+	NODE_HEREDOC,
+	NODE_AND,
+	NODE_OR
+}	t_node_type;
+
+typedef struct s_ast {
+	t_node_type		type;
+	struct s_ast	*left;
+	struct s_ast	*right;
+	char			**args;
+	char			filename;
+}	t_ast;
+
+#endif
