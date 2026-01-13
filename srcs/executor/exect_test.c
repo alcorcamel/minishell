5f79d510d4b3f1d1fbd4284176f8bc62c6567cac
Emane Daniel <emanedanielakim@gmail.com>
2026-01-13 23:23:25 +0100

    add: ajout de get path

--- srcs/executor/exect_test.c
+++ srcs/executor/exect_test.c
@@ -76,7 +76,7 @@ void print_test_result(char *name, int expected, int got)
 void test1_cmd_echo(t_shell *shell)
 {
 	//echo hello
-	t_ast *cmd = ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL});
+	t_ast *cmd = ast_cmd((char *[]){"echo", "hello", NULL});
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("1. CMD echo hello", 0, ret);
 }
@@ -85,8 +85,8 @@ void test2_pipe_echo_wc(t_shell *shell)
 {
 	// echo hello | wc -c
 	t_ast *pipe = ast_pipe(
-		ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL}),
-		ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+		ast_cmd((char *[]){"echo", "hello", NULL}),
+		ast_cmd((char *[]){"wc", "-c", NULL})
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("2. PIPE echo | wc", 0, ret);
@@ -97,10 +97,10 @@ void test3_pipe_multiple(t_shell *shell)
 	// echo hello | cat | wc -c
 	t_ast *pipe = ast_pipe(
 		ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL}),
+			ast_cmd((char *[]){"echo", "hello", NULL}),
 			ast_cmd((char *[]){"/bin/cat", NULL})
 		),
-		ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+		ast_cmd((char *[]){"wc", "-c", NULL})
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("3. PIPE echo | cat | wc", 0, ret);
@@ -110,7 +110,7 @@ void test4_redir_out(t_shell *shell)
 {
 	// echo test4 > test4.txt
 	t_ast *redir = ast_redir_out(
-		ast_cmd((char *[]){"/usr/bin/echo", "test4", NULL}),
+		ast_cmd((char *[]){"echo", "test4", NULL}),
 		"test4.txt"
 	);
 	int ret = ft_exec_root(redir, shell);
@@ -122,7 +122,7 @@ void test5_redir_in_pipe(t_shell *shell)
 	// cat | wc < test4.txt
 	t_ast *pipe = ast_pipe(
 		ast_cmd((char *[]){"/bin/cat", NULL}),
-		ast_redir_in(ast_cmd((char *[]){"/usr/bin/wc", NULL}), "test4.txt")
+		ast_redir_in(ast_cmd((char *[]){"wc", NULL}), "test4.txt")
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("5. PIPE cat | wc < file", 0, ret);
@@ -132,7 +132,7 @@ void test6_subshell_redir(t_shell *shell)
 {
 	// (echo hello) > test6.txt
 	t_ast *redir = ast_redir_out(
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL})),
+		ast_subshell(ast_cmd((char *[]){"echo", "hello", NULL})),
 		"test6.txt"
 	);
 	int ret = ft_exec_root(redir, shell);
@@ -143,8 +143,8 @@ void test7_subshell_pipe(t_shell *shell)
 {
 	// (echo hello) | (wc -c)
 	t_ast *pipe = ast_pipe(
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL})),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}))
+		ast_subshell(ast_cmd((char *[]){"echo", "hello", NULL})),
+		ast_subshell(ast_cmd((char *[]){"wc", "-c", NULL}))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("7. PIPE (echo) | (wc)", 0, ret);
@@ -154,8 +154,8 @@ void test8_pipe_subshell_redir(t_shell *shell)
 {
 	// (echo hi > test8.txt) | (wc -c)
 	t_ast *pipe = ast_pipe(
-		ast_subshell(ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "hi", NULL}), "test8.txt")),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}))
+		ast_subshell(ast_redir_out(ast_cmd((char *[]){"echo", "hi", NULL}), "test8.txt")),
+		ast_subshell(ast_cmd((char *[]){"wc", "-c", NULL}))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("8. PIPE (echo > file) | (wc)", 0, ret);
@@ -165,7 +165,7 @@ void test9_nested_subshells(t_shell *shell)
 {
 	// ((echo nested))
 	t_ast *sub = ast_subshell(
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "nested", NULL}))
+		ast_subshell(ast_cmd((char *[]){"echo", "nested", NULL}))
 	);
 	int ret = ft_exec_root(sub, shell);
 	print_test_result("9. Nested SUBSHELLS ((echo))", 0, ret);
@@ -176,11 +176,11 @@ void test10_complex_pipe_subshell(t_shell *shell)
 	// ((echo a | cat) | (wc -c | cat))
 	t_ast *pipe = ast_pipe(
 		ast_subshell(ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/echo", "a", NULL}),
+			ast_cmd((char *[]){"echo", "a", NULL}),
 			ast_cmd((char *[]){"/bin/cat", NULL})
 		)),
 		ast_subshell(ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}),
+			ast_cmd((char *[]){"wc", "-c", NULL}),
 			ast_cmd((char *[]){"/bin/cat", NULL})
 		))
 	);
@@ -193,7 +193,7 @@ void test11_cat_pipe_wc(t_shell *shell)
 	// cat | wc -l
 	t_ast *pipe = ast_pipe(
 		ast_cmd((char *[]){"/bin/cat", NULL}),
-		ast_cmd((char *[]){"/usr/bin/wc", "-l", NULL})
+		ast_cmd((char *[]){"wc", "-l", NULL})
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("11. CMD cat | wc -l", 0, ret);
@@ -203,8 +203,8 @@ void test12_echo_pipe_subshell_wc(t_shell *shell)
 {
 	// echo "hello\nworld" | (wc -l)
 	t_ast *pipe = ast_pipe(
-		ast_cmd((char *[]){"/usr/bin/echo", "hello\nworld", NULL}),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-l", NULL}))
+		ast_cmd((char *[]){"echo", "hello\nworld", NULL}),
+		ast_subshell(ast_cmd((char *[]){"wc", "-l", NULL}))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("12. PIPE echo | (wc -l)", 0, ret);
@@ -215,7 +215,7 @@ void test13_pipe_redir_in_out(t_shell *shell)
 	// < test4.txt cat | wc -c > test13.txt
 	t_ast *pipe = ast_pipe(
 		ast_redir_in(ast_cmd((char *[]){"/bin/cat", NULL}), "test4.txt"),
-		ast_redir_out(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}), "test13.txt")
+		ast_redir_out(ast_cmd((char *[]){"wc", "-c", NULL}), "test13.txt")
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("13. PIPE < file | wc > file", 0, ret);
@@ -225,8 +225,8 @@ void test14_subshell_pipe_redir(t_shell *shell)
 {
 	// (echo hi > test14.txt) | ((wc -c) > test14b.txt)
 	t_ast *pipe = ast_pipe(
-		ast_subshell(ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "hi", NULL}), "test14.txt")),
-		ast_redir_out(ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})), "test14b.txt")
+		ast_subshell(ast_redir_out(ast_cmd((char *[]){"echo", "hi", NULL}), "test14.txt")),
+		ast_redir_out(ast_subshell(ast_cmd((char *[]){"wc", "-c", NULL})), "test14b.txt")
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("14. PIPE (echo > file) | (wc > file)", 0, ret);
@@ -237,10 +237,10 @@ void test15_double_pipe_subshell(t_shell *shell)
 	// ((echo x) | (cat)) | (wc -c)
 	t_ast *pipe = ast_pipe(
 		ast_pipe(
-			ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "x", NULL})),
+			ast_subshell(ast_cmd((char *[]){"echo", "x", NULL})),
 			ast_subshell(ast_cmd((char *[]){"/bin/cat", NULL}))
 		),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}))
+		ast_subshell(ast_cmd((char *[]){"wc", "-c", NULL}))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("15. Double PIPE + SUBSHELLS", 0, ret);
@@ -250,7 +250,7 @@ void test16_pipe_cat_subshell(t_shell *shell)
 {
 	// echo "signal test" | (cat)
 	t_ast *pipe = ast_pipe(
-		ast_cmd((char *[]){"/usr/bin/echo", "signal test", NULL}),
+		ast_cmd((char *[]){"echo", "signal test", NULL}),
 		ast_subshell(ast_cmd((char *[]){"/bin/cat", NULL}))
 	);
 	int ret = ft_exec_root(pipe, shell);
@@ -262,10 +262,10 @@ void test17_nested_pipe_subshell_redir(t_shell *shell)
 	// ((echo nested | cat) > test17.txt) | ((wc -c) > test17b.txt)
 	t_ast *pipe = ast_pipe(
 		ast_subshell(ast_redir_out(ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/echo", "nested", NULL}),
+			ast_cmd((char *[]){"echo", "nested", NULL}),
 			ast_cmd((char *[]){"/bin/cat", NULL})
 		), "test17.txt")),
-		ast_subshell(ast_redir_out(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}), "test17b.txt"))
+		ast_subshell(ast_redir_out(ast_cmd((char *[]){"wc", "-c", NULL}), "test17b.txt"))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("17. Nested PIPE + SUBSHELLS + REDIR", 0, ret);
@@ -276,11 +276,11 @@ void test18_multiple_pipes_subshells(t_shell *shell)
 	// ((echo multi) | (cat)) | ((wc -c) | (cat))
 	t_ast *pipe = ast_pipe(
 		ast_pipe(
-			ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "multi", NULL})),
+			ast_subshell(ast_cmd((char *[]){"echo", "multi", NULL})),
 			ast_subshell(ast_cmd((char *[]){"/bin/cat", NULL}))
 		),
 		ast_pipe(
-			ast_subshell(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})),
+			ast_subshell(ast_cmd((char *[]){"wc", "-c", NULL})),
 			ast_subshell(ast_cmd((char *[]){"/bin/cat", NULL}))
 		)
 	);
@@ -292,7 +292,7 @@ void test19_subshell_redir_pipe(t_shell *shell)
 {
 	// (echo hello > test19.txt) | cat
 	t_ast *pipe = ast_pipe(
-		ast_subshell(ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL}), "test19.txt")),
+		ast_subshell(ast_redir_out(ast_cmd((char *[]){"echo", "hello", NULL}), "test19.txt")),
 		ast_cmd((char *[]){"/bin/cat", NULL})
 	);
 	int ret = ft_exec_root(pipe, shell);
@@ -304,13 +304,13 @@ void test20_complex_nested(t_shell *shell)
 	// ((echo final > test20.txt) | (cat | wc -c)) | (wc -c < test20.txt)
 	t_ast *pipe = ast_pipe(
 		ast_pipe(
-			ast_subshell(ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "final", NULL}), "test20.txt")),
+			ast_subshell(ast_redir_out(ast_cmd((char *[]){"echo", "final", NULL}), "test20.txt")),
 			ast_subshell(ast_pipe(
 				ast_cmd((char *[]){"/bin/cat", NULL}),
-				ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+				ast_cmd((char *[]){"wc", "-c", NULL})
 			))
 		),
-		ast_subshell(ast_redir_in(ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL}), "test20.txt"))
+		ast_subshell(ast_redir_in(ast_cmd((char *[]){"wc", "-c", NULL}), "test20.txt"))
 	);
 	int ret = ft_exec_root(pipe, shell);
 	print_test_result("20. Complex nested PIPE + SUBSHELLS + REDIR", 0, ret);
@@ -320,8 +320,8 @@ void test21_and_success(t_shell *shell)
 {
 	// echo hello && echo world
 	t_ast *cmd = ast_and(
-		ast_cmd((char *[]){"/usr/bin/echo", "hello", NULL}),
-		ast_cmd((char *[]){"/usr/bin/echo", "world", NULL})
+		ast_cmd((char *[]){"echo", "hello", NULL}),
+		ast_cmd((char *[]){"echo", "world", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("21. AND success", 0, ret);
@@ -332,7 +332,7 @@ void test22_and_fail(t_shell *shell)
 	// false && echo wontexecute
 	t_ast *cmd = ast_and(
 		ast_cmd((char *[]){"/bin/false", NULL}),
-		ast_cmd((char *[]){"/usr/bin/echo", "wontexecute", NULL})
+		ast_cmd((char *[]){"echo", "wontexecute", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("22. AND fail", 1, ret); // false retourne 1
@@ -343,7 +343,7 @@ void test23_or_success(t_shell *shell)
 	// true || echo notexecuted
 	t_ast *cmd = ast_or(
 		ast_cmd((char *[]){"/bin/true", NULL}),
-		ast_cmd((char *[]){"/usr/bin/echo", "notexecuted", NULL})
+		ast_cmd((char *[]){"echo", "notexecuted", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("23. OR success", 0, ret);
@@ -354,7 +354,7 @@ void test24_or_fail(t_shell *shell)
 	// false || echo executed
 	t_ast *cmd = ast_or(
 		ast_cmd((char *[]){"/bin/false", NULL}),
-		ast_cmd((char *[]){"/usr/bin/echo", "executed", NULL})
+		ast_cmd((char *[]){"echo", "executed", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("24. OR fail", 0, ret);
@@ -365,10 +365,10 @@ void test25_and_pipe(t_shell *shell)
 	// echo a | wc && echo b
 	t_ast *cmd = ast_and(
 		ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/echo", "a", NULL}),
-			ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+			ast_cmd((char *[]){"echo", "a", NULL}),
+			ast_cmd((char *[]){"wc", "-c", NULL})
 		),
-		ast_cmd((char *[]){"/usr/bin/echo", "b", NULL})
+		ast_cmd((char *[]){"echo", "b", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("25. AND with pipe", 0, ret);
@@ -380,9 +380,9 @@ void test26_or_pipe_fail(t_shell *shell)
 	t_ast *cmd = ast_or(
 		ast_pipe(
 			ast_cmd((char *[]){"/bin/false", NULL}),
-			ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+			ast_cmd((char *[]){"wc", "-c", NULL})
 		),
-		ast_cmd((char *[]){"/usr/bin/echo", "executed", NULL})
+		ast_cmd((char *[]){"echo", "executed", NULL})
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("26. OR with pipe and fail", 0, ret);
@@ -392,8 +392,8 @@ void test27_and_subshell(t_shell *shell)
 {
 	// (echo hi) && (echo bye)
 	t_ast *cmd = ast_and(
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "hi", NULL})),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "bye", NULL}))
+		ast_subshell(ast_cmd((char *[]){"echo", "hi", NULL})),
+		ast_subshell(ast_cmd((char *[]){"echo", "bye", NULL}))
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("27. AND with subshells", 0, ret);
@@ -404,7 +404,7 @@ void test28_or_subshell_fail(t_shell *shell)
 	// (false) || (echo executed)
 	t_ast *cmd = ast_or(
 		ast_subshell(ast_cmd((char *[]){"/bin/false", NULL})),
-		ast_subshell(ast_cmd((char *[]){"/usr/bin/echo", "executed", NULL}))
+		ast_subshell(ast_cmd((char *[]){"echo", "executed", NULL}))
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("28. OR with subshells fail", 0, ret);
@@ -415,12 +415,12 @@ void test29_complex_and_or(t_shell *shell)
 	// (echo a | wc) && (false || echo executed)
 	t_ast *cmd = ast_and(
 		ast_pipe(
-			ast_cmd((char *[]){"/usr/bin/echo", "a", NULL}),
-			ast_cmd((char *[]){"/usr/bin/wc", "-c", NULL})
+			ast_cmd((char *[]){"echo", "a", NULL}),
+			ast_cmd((char *[]){"wc", "-c", NULL})
 		),
 		ast_or(
 			ast_cmd((char *[]){"/bin/false", NULL}),
-			ast_cmd((char *[]){"/usr/bin/echo", "executed", NULL})
+			ast_cmd((char *[]){"echo", "executed", NULL})
 		)
 	);
 	int ret = ft_exec_root(cmd, shell);
@@ -432,10 +432,10 @@ void test30_and_or_subshell_redir(t_shell *shell)
 	// ((echo a > file1) && (echo b > file2)) || (echo c > file3)
 	t_ast *cmd = ast_or(
 		ast_and(
-			ast_redir_out(ast_cmd((char *[]){"/usr/bin/cat", NULL}), "file1"),
-			ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "b", NULL}), "file2")
+			ast_redir_out(ast_cmd((char *[]){"cat", NULL}), "file1"),
+			ast_redir_out(ast_cmd((char *[]){"echo", "b", NULL}), "file2")
 		),
-		ast_redir_out(ast_cmd((char *[]){"/usr/bin/echo", "c", NULL}), "file3")
+		ast_redir_out(ast_cmd((char *[]){"echo", "c", NULL}), "file3")
 	);
 	int ret = ft_exec_root(cmd, shell);
 	print_test_result("30. AND/OR with subshells and redir", 0, ret);
