fddacf7c8924018cbeefdd186d902115a58c04c1
Emane Daniel <emanedanielakim@gmail.com>
2026-01-17 16:07:08 +0100

    fix: suppression de l ajout de la difinition de throw error dans append

--- srcs/executor/exec_append.c
+++ srcs/executor/exec_append.c
@@ -1,15 +1,13 @@
 
 #include "../../includes/executor.h"
 
-void	ft_throw_error(char *err_p);
-
 int	ft_exec_append(t_ast *node, t_shell *shell)
 {
 	int	fd;
 
 	fd = open(node->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd == -1)
-		ft_throw_error(node->filename);
+		return (ft_throw_error(node->filename));
 	dup2(fd, STDOUT_FILENO);
 	close(fd);
 	return (ft_exec_ast(node->left, shell));
