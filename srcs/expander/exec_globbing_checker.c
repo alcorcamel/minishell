da64b611cd8622119e5ac4fe7b6daf0825498325
Remi Bourdon <rbourdon@f6r8s4.clusters.42paris.fr>
2026-01-20 13:51:23 +0100

    code update

--- srcs/expander/exec_globbing_checker.c
+++ srcs/expander/exec_globbing_checker.c
@@ -41,6 +41,42 @@ void	ft_args_printer2(t_ast *n)
 	}
 }
 
+int	ft_arg_restorer(char *arg)
+{
+	int	i;
+	int	size;
+
+	i = -1;
+	if (!arg)
+		return (0);
+	size = ft_strlen(arg);
+	while (++i < size)
+	{
+		if (arg[i] == '\x1D')
+			arg[i] = '*';
+	}
+	return (1);
+}
+
+int	ft_spechar_counter(char *arg)
+{
+	int	i;
+	int	size;
+	int	ret;
+
+	i = -1;
+	ret = 0;
+	if (!arg)
+		return (0);
+	size = ft_strlen(arg);
+	while (++i < size)
+	{
+		if (arg[i] == '\x1D')
+			ret++;
+	}
+	return (ret);
+}
+
 int	ft_arg_lstsize(t_new_args *lst)
 {
 	int	i;
@@ -71,7 +107,7 @@ void	ft_arg_add_back(t_new_args **lst, t_new_args *new)
 	temp->next = new;
 }
 
-int	ft_only_one_star(t_new_args **head)
+int	ft_only_one_star(t_new_args **head, char *arg)
 {
 	DIR*			rep;
 	struct dirent	*readfile;
@@ -100,9 +136,54 @@ int	ft_only_one_star(t_new_args **head)
 	}
 	if (found == 0)
 	{
-		//arg replacer et push
+		ft_arg_restorer(arg);
+		tmp = ft_argnew(ft_strdup(arg));
+		if (!tmp->value)
+			return (closedir(rep), 0);
+		ft_arg_add_back(head, tmp);
 	}
 	closedir(rep);
+	return (1);
+}
+
+int	ft_valid_star_any(char *line, char *arg)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (arg[0] == '\x1D' && line[0] == '.')
+		return (0);
+	while (arg[j])
+	{
+		if (arg[j] != '\x1D')
+		{
+			while (arg[j] == line[i])
+			{
+				i++;
+				j++;
+			}
+			if (i >= ft_strlen(line) && j >= ft_strlen(arg))
+			// if (!arg[j] && !line[i])
+				return (1);
+			if (arg[j] && arg[j] != line[i] && arg[j] != '\x1D')
+				return (0);
+			//printf("--%s: line=%s et arg=%s \n", line, line + i, arg + j);
+		}
+		else
+		{
+			while (arg[j] == '\x1D')
+				j++;
+			if (!arg[j])
+				return (1);
+			while (line[i] && line[i] != arg[j])
+				i++;
+			//printf("%s: line=%s et arg=%s \n", line, line + i, arg + j);
+			if (line[i] != arg[j])
+				return (0);
+		}
+	}
 	return (1);
 }
 
@@ -123,32 +204,99 @@ int	ft_valid_beg_star(char *line, char *arg)
 	return (1);
 }
 
-// int	ft_ending_star(t_new_args **head, char *arg)
-// {
-// 	DIR*			rep;
-// 	struct dirent	*readfile;
-// 	t_new_args		*tmp;
+int	ft_valid_end_star(char *line, char *arg)
+{
+	int	i;
+	int	j;
 
-// 	rep = NULL;
-// 	readfile = NULL;
-// 	rep = opendir(".");
-// 	if (!rep)
-// 		return (0);
-// 	while (1)
-// 	{
-// 		readfile = readdir(rep);
-// 		if (readfile == NULL)
-// 			break ;
-// 		if (ft_valid_end_star(readfile->d_name, arg))
-// 		{
-// 			tmp = ft_argnew(ft_strdup(readfile->d_name));
-// 			if (!tmp->value)
-// 				return (0);
-// 			ft_arg_add_back(head, tmp);
-// 		}
-// 	}
-// 	return (1);
-// }
+	i = 0;
+	j = 0;
+	while (arg[j] != '\x1D')
+	{
+		if (arg[j] != line[i])
+			return (0);
+		j++;
+		i++;
+	}
+	return (1);
+}
+
+int	ft_star_anywhere(t_new_args **head, char *arg)
+{
+	DIR*			rep;
+	struct dirent	*readfile;
+	t_new_args		*tmp;
+	int				found;
+
+	found = 0;
+	rep = NULL;
+	readfile = NULL;
+	rep = opendir(".");
+	if (!rep)
+		return (0);
+	while (1)
+	{
+		readfile = readdir(rep);
+		if (readfile == NULL)
+			break ;
+		if (ft_valid_star_any(readfile->d_name, arg))
+		{
+			tmp = ft_argnew(ft_strdup(readfile->d_name));
+			found = 1;
+			if (!tmp->value)
+				return (0);
+			ft_arg_add_back(head, tmp);
+		}
+	}
+	if (found == 0)
+	{
+		ft_arg_restorer(arg);
+		tmp = ft_argnew(ft_strdup(arg));
+		if (!tmp->value)
+			return (closedir(rep), 0);
+		ft_arg_add_back(head, tmp);
+	}
+	closedir(rep);
+	return (1);
+}
+
+int	ft_ending_star(t_new_args **head, char *arg)
+{
+	DIR*			rep;
+	struct dirent	*readfile;
+	t_new_args		*tmp;
+	int				found;
+
+	found = 0;
+	rep = NULL;
+	readfile = NULL;
+	rep = opendir(".");
+	if (!rep)
+		return (0);
+	while (1)
+	{
+		readfile = readdir(rep);
+		if (readfile == NULL)
+			break ;
+		if (ft_valid_end_star(readfile->d_name, arg))
+		{
+			tmp = ft_argnew(ft_strdup(readfile->d_name));
+			if (!tmp->value)
+				return (0);
+			ft_arg_add_back(head, tmp);
+		}
+	}
+	if (found == 0)
+	{
+		ft_arg_restorer(arg);
+		tmp = ft_argnew(ft_strdup(arg));
+		if (!tmp->value)
+			return (closedir(rep), 0);
+		ft_arg_add_back(head, tmp);
+	}
+	closedir(rep);
+	return (1);
+}
 
 int	ft_beginning_star(t_new_args **head, char *arg)
 {
@@ -182,7 +330,11 @@ int	ft_beginning_star(t_new_args **head, char *arg)
 	}
 	if (found == 0)
 	{
-		//arg replacer et push
+		ft_arg_restorer(arg);
+		tmp = ft_argnew(ft_strdup(arg));
+		if (!tmp->value)
+			return (closedir(rep), 0);
+		ft_arg_add_back(head, tmp);
 	}
 	closedir(rep);
 	return (1);
@@ -196,22 +348,14 @@ int	ft_args_splitter(t_new_args **head, char *arg)
 	rep = NULL;
 	if (!arg)
 		return (closedir(rep), 0);
-	if (ft_strlen(arg) == 1)
-		return (ft_only_one_star(head));
-	else if (arg[0] && arg[0] == '\x1D')
-		return (ft_beginning_star(head, arg));
-	// else if (arg[ft_strlen(arg) - 1] == '\x1D')
+	// if (ft_strlen(arg) == 1)
+	// 	return (ft_only_one_star(head, arg));
+	// else if (arg[0] == '\x1D' && ft_spechar_counter(arg) == 1)
+	// 	return (ft_beginning_star(head, arg));
+	// else if (arg[ft_strlen(arg) - 1] == '\x1D' && ft_spechar_counter(arg) == 1)
 	// 	return (ft_ending_star(head, arg));
-	readfile = NULL;
-	// rep = opendir(".");
-	// if (!rep)
-	// 	return (0);
-	// while (1)
-	// {
-	// 	readfile = readdir(rep);
-	// 	if (readfile == NULL)
-	// 		break ;
-	// }
+	// else
+		return (ft_star_anywhere(head, arg));
 	return (1);
 }
 
@@ -253,7 +397,7 @@ int		ft_args_handler(t_ast *n)
 	head = NULL;
 	n->globber = head;
 	if (n->args[0] && n->args[0][0] == '\x1D')
-		n->args[0][0] = '*';// remplacer toutes les etoiles ds arg[0]
+		ft_arg_restorer(n->args[0]);
 	while (n->args[++i])
 	{
 		if (ft_strchr(n->args[i], (int)'\x1D'))
