fe963df536886c30c1b3f9344da656efb63bd090
Remi Bourdon <rbourdon@f6r8s4.clusters.42paris.fr>
2026-01-22 15:17:40 +0100

    code update

--- srcs/expander/expander_error.c
+++ srcs/expander/expander_error.c
@@ -3,5 +3,18 @@
 void	ft_expander_error(char *s, int i)
 {
 	if (i == 1)
-		printf("bash: %s: ambigous redirect", s);
+		printf("bash: %s: ambigous redirect\n", s);
+}
+
+void	ft_free_nargs(t_new_args *node)
+{
+	t_new_args	*next;
+
+	while (node)
+	{
+		next = node->next;
+		free(node->value);
+		free(node);
+		node = next;
+	}
 }
