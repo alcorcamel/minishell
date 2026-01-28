3034014d8a1be20ca67f286fda4236ee04e5d8cc
Remi Bourdon <rbourdon@f6r8s4.clusters.42paris.fr>
2026-01-28 14:08:35 +0100

    code update

--- srcs/expander/expander.c
+++ srcs/expander/expander.c
@@ -1,24 +1,24 @@
 #include "../../includes/expander.h"
 
-static int	ft_rebuild_node(t_ast *n)
-{
-	if (n->type == NODE_REDIR_IN || n->type == NODE_REDIR_OUT
-		|| n->type == NODE_REDIR_APPEND)
-	{
-		if (!ft_redir_rebuild(n))
-			return (0);
-	}
-	return (1);
-}
+// static int	ft_rebuild_node(t_ast *n)
+// {
+// 	if (n->type == NODE_REDIR_IN || n->type == NODE_REDIR_OUT
+// 		|| n->type == NODE_REDIR_APPEND)
+// 	{
+// 		if (!ft_redir_rebuild(n))
+// 			return (0);
+// 	}
+// 	return (1);
+// }
 
 static int	ft_expand_node(t_ast *n, t_shell *shell)
 {
-	if (n->type == NODE_REDIR_IN || n->type == NODE_REDIR_OUT
-		|| n->type == NODE_REDIR_APPEND)
-	{
-		if (!ft_redir_expand(n, shell))
-			return (0);
-	}
+	// if (n->type == NODE_REDIR_IN || n->type == NODE_REDIR_OUT
+	// 	|| n->type == NODE_REDIR_APPEND)
+	// {
+	// 	if (!ft_redir_expand(n, shell))
+	// 		return (0);
+	// }
 	if (n->type == NODE_HEREDOC)
 	{
 		if (!ft_heredoc_rebuild(n))
@@ -36,8 +36,8 @@ int	ft_explore_ast(t_ast **root, t_shell *shell)
 	n = *root;
 	if (!ft_expand_node(n, shell))
 		return (0);
-	if (!ft_rebuild_node(n))
-		return (0);
+	// if (!ft_rebuild_node(n))
+	// 	return (0);
 	if (n->left && !ft_explore_ast(&n->left, shell))
 		return (0);
 	if (n->right && !ft_explore_ast(&n->right, shell))
